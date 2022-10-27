cfb15fc7b9648b53ffc496ee64df3448ea903956
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-10-27 13:09:55 +0200

    coloring of grid possible

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function addGrid() {
+window.onload = function addGrid() {
     const container = document.getElementById("grid");
 
     for (let i = 0; i < 256; i++) {
         const newDiv = document.createElement("div");
+        newDiv.classList.add("box");
         container.appendChild(newDiv)
     }
 }
 
-addGrid();
-
-
-
-// Cloning Nodes
-//    const clone = newDiv.cloneNode(false);
-//    container.appendChild(clone);
+document.addEventListener("mouseover", function hover(e) {
+    if (e.target.className === "box"){
+        console.log(e.target.className)
+        e.target.style.background = "black";
+    }
+})
