f6442fadceaea8f6bda045c85bc1f9a7fd8f8532
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-10-28 12:07:54 +0200

    implemented slider functionality

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
+const container = document.getElementById("grid");
 const root = document.querySelector(":root");
 const columns = getComputedStyle(root).getPropertyValue("--column-number");
 const rows = getComputedStyle(root).getPropertyValue("--row-number");
-const cells = rows * columns;
+let cells = rows * columns;
+const slider = document.getElementById("myRange");
+const value = document.getElementById("value");
 
-
-window.onload = function addGrid() {
-    const container = document.getElementById("grid");
-
-    for (let i = 0; i < cells; i++) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("box");
-        container.appendChild(newDiv)
-    }
+window.onload = function load() {
+    value.innerHTML = `${columns} x ${rows}`;
+    newGrid();
 }
 
 document.addEventListener("mouseover", function hover(e) {
     if (e.target.className === "box"){
-        console.log(e.target.className)
         e.target.style.background = "black";
     }
 })
+
+slider.oninput = function slider() {
+    value.innerHTML = `${this.value} x ${this.value}`;
+    clearGrid();
+    newGrid();
+}
+
+function clearGrid() {
+    container.innerHTML = "";
+}
+
+function newGrid() {
+    cells = slider.value * slider.value;
+
+    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`
+
+    for (let i = 0; i < cells; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("box");
+        container.appendChild(newDiv);
+    }
+}
