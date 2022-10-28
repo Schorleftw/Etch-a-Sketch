8ade147035da4112f3d7734d1ed7fe9cc3e67656
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-10-28 10:00:58 +0200

    implemented dynamic row and column creation inside div

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const root = document.querySelector(":root");
+const columns = getComputedStyle(root).getPropertyValue("--column-number");
+const rows = getComputedStyle(root).getPropertyValue("--row-number");
+const cells = rows * columns;
+
+
 window.onload = function addGrid() {
     const container = document.getElementById("grid");
 
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < cells; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("box");
         container.appendChild(newDiv)
